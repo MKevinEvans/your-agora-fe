fd29355ef548f18251519e43184fba7d7e6de0f9
mkevinevans <mkevinevans@gmail.com>
2016-12-13 18:17:56 -0500

    change url

--- src/actions/resetUserLean.js
+++ src/actions/resetUserLean.js
@@ -3,7 +3,7 @@ import $ from 'jquery'
 export default function resetUserLean(){
   return function(dispatch){
     $.ajax({
-      url: `http://localhost:3000/leaning/reset`,
+      url: `https://youragora.herokuapp.com/leaning/reset`,
       type: "GET",
       headers: {authorization: localStorage.getItem('jwt')},
       contentType: 'application/json; charset=utf-8',
