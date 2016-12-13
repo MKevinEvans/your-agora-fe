7793ab1af4088c10246ae2860f9f353d306a0801
mkevinevans <mkevinevans@gmail.com>
2016-12-13 08:46:45 -0500

    see last commit

--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux'
-import rootReducer from '../reducers'
+import rootReducer from '../reducers/rootReducer'
+import thunk from 'redux-thunk'
 // import promise from 'redux-promise'
 
 const enhancer = applyMiddleware(thunk);
